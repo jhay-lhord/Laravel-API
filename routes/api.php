aeb1d8c3a7cc8551f0ec08405618ddb2857480fd
Jaylord Manila <jaylordmanila.tech@gmail.com>
2025-07-21 09:36:34 +0800

    add api version

--- routes/api.php
+++ routes/api.php
@@ -7,22 +7,22 @@ use Illuminate\Http\Request;
 
 Route::post('/login', [AuthController::class, 'login'])->name('login');
 
-Route::middleware('auth:sanctum')->group(function (){
-  Route::get('v1/user', function(Request $request){
+Route::prefix('v1')
+->middleware('auth:sanctum')->group(function (){
+  
+  Route::get('/user', function(Request $request){
     return $request->user();
   });
-
-  Route::get('v1/users', [AuthController::class, 'index']);
-
-  Route::get('v1/profile', function (Request $request) {
+  Route::get('/users', [AuthController::class, 'index']);
+  Route::get('/profile', function (Request $request) {
     return response()->json($request->user());
   });
 
-  Route::get('v1/products', [ProductController::class, 'index' ]);
-  Route::post('v1/products', [ProductController::class, 'store' ]);
-  Route::get('v1/product/{id}', [ProductController::class, 'show' ]);
-  Route::put('v1/product/{id}', [ProductController::class, 'update' ]);
-  Route::delete('v1/product/{id}', [ProductController::class, 'destroy' ]);
+  Route::get('/products', [ProductController::class, 'index' ]);
+  Route::post('/products', [ProductController::class, 'store' ]);
+  Route::get('/product/{id}', [ProductController::class, 'show' ]);
+  Route::put('/product/{id}', [ProductController::class, 'update' ]);
+  Route::delete('/product/{id}', [ProductController::class, 'destroy' ]);
 
 });
 
